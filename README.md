f929214d531ee8fe6bfc6d7850a9dd3160dec486
riccardo zanussi <rzanussi@gmail.com>
2018-08-21 22:24:05 +0200

    fourth

--- README.md
+++ README.md
@@ -1,40 +1,46 @@
-# cryptobet
+# Cryptobet is an application to generate, manage and play bets
 
+## There are two contracts: MainBet and GenericBet
+MainBet is deployed with the parameters to generate a sequence of bets. The parameters are given once and all the generated bets implement the same parameters of their MainBet. To have a sequence of bets with different parameter it is sufficient deploy another MainBet contract with different parameters. From a MainBet, a bet can be deployed only when the former bet of the same sequence is finished with a winner, with the function createBet The GenericBet contract store the parameters of the bet, initialize all the variable for bet and manage the bet of the players and the end of the bet. It the basic functions are: buyNumber, combineNumbers and endBet (internal)
 
-Cryptobet is an application to generate, manage and play bets
+## Parameters
+The parameters to generate a MainBet contract (and that are passed to the new bet) are
+* feeAccount: the account where the fee for the Manager is payed when a bet has a winner
+* feePercentage: the percentage of the bet going to the manager fee
+* newNumberPrice: the cost of buying a random number
+* betName: name of the Bet
+* numberOfNumbers: numbers can be bought (max.100)
+* numberOfPossibleWinners: length of the range of numbers that can win. (Always less the numberOfNumbers)
+* sumNumbers: sum of all the numberOfNumbers (ex: in case of numberOfNumbers = 100, this number is 5050. It is as parameters not to be computed inside a smart contract to reduce the number of loops)
 
-Roles
-There are tow roles: manager (he generate a family of bet with parameters, generate the single bet and have the power to pause it) and player (can buy a random number with a fee and combine two number to have another one)
+## Roles 
+There are two roles: manager: a manager can generate a mainbet with parameter, can generate instantiate a single bet from the mainbet and have the power to pause it player: a player interact with the instance of a bet, can buy a random number with a fee and combine two number to have another one
 
-Cryptobet is made by two contract: MainBet and GenericBet
+## Flow of the game
 
-MainBet is deployed with the parameters to generate a sequence of bets. The parameters are given once and all the generated bets implement the same parameters of their MainBet. To have a sequence of bets with different parameter it is sufficient deploy another MainBet contract with different parameters. 
-From a MainBet, a bet can be deployed only when the former bet of the same family is finished with a winner
+1. (Generate a sequence of bets) the manager instantiate the contract MainBet passing the parameters EX: Given numberOfNumbers value 100; I can choose - example - numberOfPossibleNumbers as 10. It means that just 10 out of 100 numbers can be the winner number (a winning zone). The 10 consecutive numbers are chosen randomly by the contract
+2. {Generate a bet). The manager execute the function CreateBet on the contract MainBet to create a contract GenericBet. This function is the most expensive of the application because prepare all the fields for the bet. Intentionally all the heavy executions are here, so the gas is paid by the manager From this moment player, knowing the address of the contract, can bet
+3. Player can buy a number paying a fee (establish as parameter when the sequence of bet is created). The number is chosen randomly by the contract
+4. To make the game more interesting and allow players to have strategies instead of pure luck, a function to combine two numbers is allowed. This function take two number (both owned by the player) sum them and divide for two. (if the result of the sum is not divisible for two, it is decreased by 1). So a player with numbers out of the winning zone but with one bigger and one lower, can combine to have a number in the winning zone. If the result number is already owned, the combine is unsuccessful. The two number used for the combine are set free again. The computed number is free and assigned to the message sender While buying a random number implies a fee, combine two number not: the player have already paid fee for two numbers used in the combine and he will lose these numbers. So effectively, the combined number will cost a double fee.
+5. All the fee paid for buying numbers are put in a basket 
+6)When all the number in the winning zone are bought the bet is finished by the smart contract. The contract choose randomly a number between the numbers in the winning zone and the owner of this number is the winner. A fee (equal to the feePercentage*basket amount) is given to feeAccount, all the remaining money is given to the owner of the winning number and the bet is put to Ended. The Manager can now generate another bet of the same family
 
-The parameters to generate a MainBet contract are
- - feeAccount
-   the account where the fee for the Manager is payed when a bet has a winner
- - feePercentage 
-   the percentage of the bet going to the manager fee
- - newNumberPrice
-   the cost of buying a random number 
- - betName
-   name of the Bet
- - numberOfNumbers
-   numbers can be bought (max.100)
- - numberOfPossibleWinners
-   length of the range of numbers that can win. (Always less the numberOfNumbers)
- - sumNumbers 
-   sum of all the numberOfNumbers (ex: in case of numberOfNumbers = 100, this number is 5050. I is as parameters not to be computed inside a smart contract to reduce the number of loops)
+## Truffle
 
-Flow of the game (Through an Example)
+ubuntu 16.04 node 10.4.1 truffle 4.1.11 (core 4.1.11) solidity v0.4.24 (solc.js) ganache-cli v6.1.6 (ganache-core 2.1.5) 
+### **The test are run with GANACHE-CLI**
 
-Just give numberOfNumbers value 100; I can choose - example - numberOfPossibleNumbers as 10. It means that just 10 out of 100 numbers can be the winner number (a winning zone). 
-When a player buy a number, the smart contract vie a random number in the range of 100. Of course 9/10 the bought number will be out of the range of possible winner.
+## Test
+There are 5 test The contract MainBet.sol is deployed. The contract contains the source of the two contracts the make the application : MainBet and GenericBet
 
-To make the game more interesting and allow players to have strategies instead of pure luck, a function to combine two numbers is allowed. This function take two number (both owned by the player) sum them and divide for two. (if the result of the sum is not divisible ofr two, it is decreased by 1). So a player with numbers out of the winning zone but with one bigger and one lower, can combine to have a number in the winning zone. If the result number is already owned, the combine is unsuccesful. The two number used for the combine are set free again when the computed number is free and assigned to the message sender
-While buying a random number implies a fee, combine two number not: the player have already paid fee for two numbers used in the combine and he will lose these numbers. So efectively, the combined number will cost a double fee. 
+1. The function CreateBet is called to generate the single instance of a bet the asserts tests if the contract is generated checking the address as return value of the function and as variable
+2. check is on the of the parameters in the sequence of the bet is passed and created correctly in the GenericBet contract
+3. To keep in account of the number bought, a variable is passed when the sequence of bets is created containing the sum of all the possible numbers. Anytime a number is bought, the value of the bought number is subtracted from this value. the tets execute the function buyNumber and check that the new sum of numbers values is equal to the former value less the value of the bought number
+4. the function combineNumbers is called. (to have two number to combine, a new buyNumber function is called and the number boguht by the former test is reused) the assert check is the computation of the combined number is correct and if the combined number is effectively assigned
+5. the function of buyNumber is called in a loop until the bet is finished (because all the numbers in the winning zone are bought). The contract automatically call the endBet function, choose a random number and assign a winner the assert check if the winning number is in the winning zone
 
-All the fee paid for buying numbers are put in a basket
-When all the number in the winning zone are bought the bet is finished by the smart contract, a fee (equal to the feePercentage*basket amount) is given to feeAccount, all the remaining money is given to the owner of the winning number and the bet is put to Ended. The Manager can now generate another bet
+##Web application
 
+There a web application to check some functionalities of the contracts. The web application allow to buy numbers and combine numbers. 
+The web application is made completely in Node (i dont have a deep knowledge of javascript) for this reason I couldn’t use neither metamask neither uport 
+### **The web application rely on contracts deployed in Rinkeby]**
--- contracts/MainBet.sol
+++ contracts/MainBet.sol
@@ -2,11 +2,14 @@ pragma solidity ^0.4.23;
 
 import '../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol';
 
-
-
-
+/** @title GenericBet */ 
+/** @dev Contract managing a bet 
+     . 
+      */ 
 
 contract GenericBet { 
+      
+
     
     struct betNumber { 
         address betNumberOwner; 
@@ -21,8 +24,6 @@ contract GenericBet {
     address public owner; 
     bool public ended; 
     bool public  paused; 
-    
-   // address[] public   numberOwners; 
     uint[] public  freeNumbers; 
     uint public freeNumbersLength; 
     uint public prizeNumbers; 
@@ -42,10 +43,8 @@ contract GenericBet {
         require(msg.sender == owner); 
         _; 
     } 
-    
     uint public resultNumber; 
-
-
+    
     event numberIsBought(uint, address); 
     event numberIsCombined(uint, address); 
     event numberIsNotCombined(uint, address); 
@@ -79,7 +78,16 @@ contract GenericBet {
       
 
 
-    // Constructor,set all the necesary variables 
+   /** @dev Constructor for genericBet. 
+      * @param _feeAccount  Address where the fee is paid. 
+      * @param _feePercentage To compute the fee to _feeAccount 
+      * @param _newNumberPrice Fee to pay to buy a random number 
+      * @param _numberOfNumbers  Total numbers can be bought 
+      * @param _numberOfPossibleWinners Number of numbers that are possible winners. it is always less than _numnerOfNumbers 
+      * @param _sumNumbers  Sum of all the numbers from 1 to _numberOfNumbers. ex: for _numbersOfNumbers = 100, this have to be 5050 
+      * @param _owner Account of the manager 
+      */ 
+
     constructor(address _feeAccount,  uint _feePercentage, 
         uint _newNumberPrice, uint _numberOfNumbers, uint _numberOfPossibleWinners, uint  _sumNumbers, address _owner) public { 
     
@@ -110,14 +118,15 @@ contract GenericBet {
         for(uint i=0;i<numberOfNumbers;i++) 
         { 
             freeNumbers.push(i+1); 
-            // numberOwners.push(0x0); 
             betNumbers[i+1] = betNumber(0x0,i); 
         } 
         
 
 
     } 
-    // the bought numbers are assigned randomly 
+    /** @dev buyNumber for genericBet. 
+      * @return  _bought number. 
+      */ 
     function buyNumber() public payable  notEnded notPaused enoughFee returns (uint) { 
       
     
@@ -134,7 +143,7 @@ contract GenericBet {
          indexBoughtNumber = randomUint(thisAddress, numbersSum, freeNumbersLength - 1); 
         boughtNumber = freeNumbers[indexBoughtNumber]; 
          } 
-          
+        // add the prize of a number to the bet prize. made by parameters not by value 
         betPrize += newNumberPrice; 
         
         
@@ -168,28 +177,35 @@ contract GenericBet {
 
     // a player can combine two numbers in another number (average) to try to have a number in the winner range 
     // if the resulting number is free is assigned to the sender. But the two number used, are freed 
+     /** @dev combbineNumber for genericBet. 
+      * @param number1 first of the numbers used to find the number to bought 
+      * @param number2 second  of the numbers used to find the number to bought 
+      * @return  newNumebr number given to the sender combined by the two number in input. 
+      */ 
     function combineNumbers(uint number1, uint number2) public notEnded notPaused 
                     numberOwner(number1) numberOwner(number2) returns(uint newNumber){ 
-        sumNumbers = number1 + number2; 
+        sumNumbers = (SafeMath.add(number1 , number2)); 
+        // check if the usm of two numbers are even amd compute the average 
         if(sumNumbers%2 == 1){ 
             sumNumbers -= 1; 
         } 
         resultNumber = sumNumbers / 2; 
 
-
+        // if the resulting number is free.. 
         if(betNumbers[resultNumber].betNumberOwner == 0x0){ 
-            //numberOwners[number1] = 0x0; 
-            //numberOwners[number2] = 0x0; 
             
-            // numberOwners[resultNumber - 1] = msg.sender; 
-            
+            // remove result number from free numbers and put the sender as owner 
             remove(betNumbers[resultNumber].betNumberIndexFreeNumbers); 
             betNumbers[resultNumber] = betNumber(msg.sender,999); 
+            
+            // free the two input numbers and set owner as 0x0 
             freeNumbers[freeNumbersLength] = number1; 
             betNumbers[number1] = betNumber(0x0, freeNumbersLength); 
             freeNumbers[freeNumbersLength + 1] = number2; 
             betNumbers[number2] = betNumber(0x0, freeNumbersLength + 1); 
             freeNumbersLength += 2; 
+            
+            //update the number of free number left in winner zone 
              if(resultNumber >= prizeNumberLow && resultNumber <= prizeNumberHigh){ 
             prizeNumbers -= 1; 
         } 
@@ -207,7 +223,8 @@ contract GenericBet {
     } 
 
 
-
+        /** @dev endBet for genericBet. all the numbers in winner zone are bought. Find the winner 
+        */ 
         function endBet() private notEnded notPaused { 
             // set the flag to ended and pause (for security) 
             ended = true; 
@@ -218,13 +235,13 @@ contract GenericBet {
             // winnerNumber = randomUintAA(thisAddress, numberOwners[boughtNumber - 1], numberOfPossibleWinners); 
             winnerNumber += uint(prizeNumberLow); 
             
-            // tranfer money to the winner except for the fee 
+            // tranfer money to the winner except for the fee. just keep it reentrancy safe 
             prize = betPrize; 
             uint prizeForEvent = betPrize; 
             betPrize = 0; 
             
             feeAccount.transfer(prize * feePercentage /100); 
-           // numberOwners[winnerNumber - 1].transfer(prize * (100 - feePercentage)  /100); 
+            
             betNumber storage  betNumberWinner = betNumbers[winnerNumber]; 
             uint winnerPrize = prize * (100 - feePercentage) / 100; 
             betNumberWinner.betNumberOwner.transfer(winnerPrize); 
@@ -237,7 +254,10 @@ contract GenericBet {
 
         } 
         // remove a number for the array of free numbers 
-    function remove(uint index) private   { 
+        /** @dev remove for genericBet. 
+         * @param index  number to remove from the array of free numbers 
+        */ 
+        function remove(uint index) private   { 
         require(index <= freeNumbersLength); 
         freeNumbers[index] = freeNumbers[freeNumbersLength - 1]; 
         betNumbers[freeNumbers[freeNumbersLength - 1]] = betNumber(0x0,index);  /////////// 
@@ -255,9 +275,9 @@ contract GenericBet {
         emit restarted(thisAddress); 
     } 
 
-
+    // polymorphism dont used. Too new feature 
     
-    // random function with an address and one number) 
+    // random function with an address and one number 
     function randomUint(address seed1, uint seed2, uint modulo) private pure returns (uint) { 
       return uint(uint256(keccak256(abi.encodePacked(seed1, seed2)))%modulo);   } 
 
@@ -287,22 +307,29 @@ contract MainBet {
     uint public numberOfPossibleWinners; 
     uint public sumNumbers; // sum of the numbers from 1 to 100 
     string betName; 
-    
-
-
-/* addrees of the contract of the actual bet. only one bet at time is allowed */ 
+    /* addrees of the contract of the actual bet. only one bet at time is allowed */ 
     address public actualBetContractAddress; 
     GenericBet actualBetContract; 
     address public owner ; 
- modifier ownerOnly { 
+    
+    modifier ownerOnly { 
         require(msg.sender == owner); 
         _; 
     } 
-    // event bet created ok 
+      
     event betCreated(address actualBetContractAddress); 
 
 
-    // Constructor     
+    // Constructor 
+     /** @dev Constructor for mainBet. 
+      * @param _feeAccount  Address where the fee is paid. 
+      * @param _feePercentage To compute the fee to _feeAccount 
+      * @param _newNumberPrice Fee to pay to buy a random number 
+      * @param _betName  Nmae of the bet. just for display 
+      * @param _numberOfNumbers  Total numbers can be bought 
+      * @param _numberOfPossibleWinners Number of numbers that are possible winners. it is always less than _numnerOfNumbers 
+      * @param _sumNumbers  Sum of all the numbers from 1 to _numberOfNumbers. ex: for _numbersOfNumbers = 100, this have to be 5050 
+      */ 
     constructor(address _feeAccount,  uint _feePercentage, 
         uint _newNumberPrice, string _betName, uint _numberOfNumbers, uint _numberOfPossibleWinners, uint _sumNumbers ) public { 
         /* Set the bets variable of this generation of cruptobet */ 
@@ -323,7 +350,9 @@ contract MainBet {
         
     } 
 
-
+     /** @dev createBet for mainBet. 
+      * @return address of the generated contract 
+      */ 
     function createBet() public ownerOnly returns(address) { 
         /**  test if address is zero for the first time */ 
         if( (address(actualBetContract) != 0x0 && actualBetContract.ended() ) || address(actualBetContract) == 0x0){ 
@@ -337,9 +366,4 @@ contract MainBet {
     } 
 
 
- // function randomAddress(address seed, uint modulo) private pure returns (uint) { 
-   //   return uint(uint256(keccak256(abi.encodePacked(seed)))%modulo);   } 
-
-
     } 
-
